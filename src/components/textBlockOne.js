393455a3db06c51b67f708a57369bb151480145b
Craig W <cwheeler@nicopure.com>
2018-03-14 17:14:12 -0400

    style updates to TextBlockThree

--- src/components/textBlockOne.js
+++ src/components/textBlockOne.js
@@ -12,15 +12,15 @@ const TextBlockOne = () => (
   <Item>
     <Box title="TextBlockOne" cellSpacing={25} align="center" class="mktEditable" id="text_block_1">
       <Item align="center">
-        <h2 style={{textAlign: 'center'}}><Span {...sectionText}>Quality Base. Value Price.</Span></h2>
+        <h2 style={{textAlign: 'center'}}><Span {...sectionText} style={{fontSize: 26}}>Quality Base. Value Price.</Span></h2>
         <p style={{textAlign: 'center'}}><Span {...sectionText}>Fusion nic shots are the perfect complement for Shake N Vapes. Available in TPD-registered and compliant 10ml bottles, Fusion nic shots are made to meet rigorous quality standards and provide a great value. Here are just a few of Fusion’s features:</Span></p>
           <ul style={{textAlign: 'left'}}>
-            <li>Flavourless nicotine shot (20mg/ml) that can be added to Shake N Vape-style e-liquids</li>
-            <li>TPD-Registered and compliant labels</li>
-            <li>American-Made quality nicotine shot perfected in state-of-the-art labs</li>
-            <li>Pharmaceutical-style peel back label</li>
-            <li>Available in 25 countries</li>
-            <li>Minimal packaging results in less environmental impact</li>
+            <li><Span {...sectionText}>Flavourless nicotine shot (20mg/ml) that can be added to Shake N Vape-style e-liquids</Span></li>
+            <li><Span {...sectionText}>TPD-Registered and compliant labels</Span></li>
+            <li><Span {...sectionText}>American-Made quality nicotine shot perfected in state-of-the-art labs</Span></li>
+            <li><Span {...sectionText}>Pharmaceutical-style peel back label</Span></li>
+            <li><Span {...sectionText}>Available in 25 countries</Span></li>
+            <li><Span {...sectionText}>Minimal packaging results in less environmental impact</Span></li>
           </ul>
       </Item>
     </Box>
--- src/components/textBlockThree.js
+++ src/components/textBlockThree.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import {Email, Item, A, Box, Image, Span} from 'react-html-email'
 
-const sectionText = {
-  fontFamily: 'Calibri, sans-serif',
-  fontSize: 16,
-  fontWeight: 300,
-  color: '#757575',
-  lineHeight: 26,
-}
-
 const TextBlockThree = () => (
 
   <Item>
-    <Box title="TextBlockThree" cellSpacing={75} align="center">
+    <Box title="TextBlockThree" cellSpacing={75} align="center" class="mktEditable" id="text_block_3">
       <Item align="center">
-        <Span {...sectionText}>
-          <h2>Contact Us!</h2>
-          <p>Contact your account manager for additional details and pricing on Fusion nic shots.</p>
+        <h2 style={{ textAlign: 'center', fontFamily: 'Calibri, sans-serif', fontWeight: 300, color: '#757575' }}>Contact Us!</h2>
+        <p style={{ textAlign: 'center', fontFamily: 'Calibri, sans-serif', fontWeight: 300, color: '#757575', lineHeight: 1.5 }}>
+          Contact your account manager for additional details and pricing on Fusion nic shots.
+        </p>
+        <p style={{ textAlign: 'center' }}>
           <button style={{ backgroundColor: 'red', color: '#fff', height: 40, width: 200, fontSize: 16 }}>Contact Us</button>
-        </Span>
+        </p>
       </Item>
     </Box>
   </Item>
